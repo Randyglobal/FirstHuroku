7fec5aab47b645e5a5cc8b93b5c2a1019025784c
Randyglobal <randyitoe2255@gmail.com>
2022-10-04 20:36:58 -0700

    modification of index.html

---
+++ Javascript.js
@@ -0,0 +1,31 @@
+// var randy = document.getElementById('dropBtn');
+// var creat = document.getElementById('creatInfo');
+
+
+// randy.addEventListener('click', function(){
+//     if(creat.style.display == 'none'){
+//         creat.style.display = 'block'
+//     }else{
+//         creat.style.display = 'none'
+//     }
+// })
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
